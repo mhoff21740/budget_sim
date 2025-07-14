6a5220d2ae5d4abb67cd9b4810d3d56d10432d23
mhoff21740 <marshall.hoffmaster@gmail.com>
2025-07-14 15:39:44 -0400

    finished up calculations

--- personal_budget/calculations.py
+++ personal_budget/calculations.py
@@ -1,5 +1,8 @@
  # FP utilities: totals, filters, forecasts
- 
+import functools 
+from collections import defaultdict
+
+
 def total_expenses(budget):
     total_expenses = sum((map(lambda e: e["amount"], budget["expenses"])))
     return total_expenses
@@ -9,18 +12,42 @@ def total_expenses(budget):
     
 
 def remaining_balance(budget):
-    ...
+    return budget["income"] - total_expenses(budget)
+
+
+
+
+def update_totals(totals, expense):
+    category = expense["category"]
+    amount = expense["amount"]
+
+    if category in totals:
+        totals[category] += amount
+    else:
+        totals[category] = amount
+
+    return totals
+
+
 
 
 def expenses_by_category(budget):
-    ...
+    expenses = budget["expenses"]
+    totals = functools.reduce(lambda totals, expense: update_totals(totals, expense), expenses, {} )
+    
+    return totals
+
+
+
 
 
 def filter_expenses_by_catagory(budget, category):
-    ...
+    filtered_list = list(filter(lambda expense: expense["category"] == category, budget["expenses"]))
+    return filtered_list
+
 
 
     
-    
 
     
+
