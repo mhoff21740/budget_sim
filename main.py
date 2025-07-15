de2bdc46ad13a6d8c199722bba7e38ad039904d5
mhoff21740 <marshall.hoffmaster@gmail.com>
2025-07-15 12:10:35 -0400

    created main skeleton

--- main.py
+++ main.py
@@ -2,3 +2,22 @@
 # Accepts hardcoded vaules for time being, then will accept user cli inputs.
 #Will Display budget summary
 #Calls function to update state and display results 
+
+
+
+
+def main():
+    ...
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+if __name__ == "__main__":
+    main()
